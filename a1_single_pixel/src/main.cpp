be23cd2a8b02ac879a91566289c3697f9e2a7f54
Andreas Gysin <andreas@ertdfgcvb.ch>
2024-01-08 13:41:44 +0100

    Add example 1

---
+++ a1_single_pixel/src/main.cpp
@@ -0,0 +1,68 @@
+/**
+ * Control a single LED of a RGB matrix, directly from the controller.
+ *
+ * The SmartMatrix library offers many tools (and examples) to display graphics,
+ * animations and texts.
+ * Dependencies (and docs):
+ * https://github.com/pixelmatix/SmartMatrix
+ *
+ * Fork of the library that allows control of the special 32x32 matrix
+ * https://github.com/Kameeno/SmartMatrix
+ */
+
+// Pinout configuration for the PicoDriver v.5.0
+#include "common/pico_driver_v5_pinout.h"
+
+#include <Arduino.h>
+#include <SmartMatrix.h>
+
+#define COLOR_DEPTH 24   // valid: 24, 48
+#define TOTAL_WIDTH 32   // Size of the total (chained) with of the matrix/matrices
+#define TOTAL_HEIGHT 32  // Size of the total (chained) height of the matrix/matrices
+#define kRefreshDepth 24 // Valid: 24, 36, 48
+#define kDmaBufferRows 4 // Valid: 2-4
+#define kPanelType SM_PANELTYPE_HUB75_32ROW_32COL_MOD8SCAN // custom
+#define kMatrixOptions (SM_HUB75_OPTIONS_NONE)
+#define kbgOptions (SM_BACKGROUND_OPTIONS_NONE)
+
+// SmartMatrix setup & buffer alloction
+SMARTMATRIX_ALLOCATE_BUFFERS(matrix, TOTAL_WIDTH, TOTAL_HEIGHT, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
+
+// A single background layer "bg"
+SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(bg, TOTAL_WIDTH, TOTAL_HEIGHT, COLOR_DEPTH, kbgOptions);
+
+void setup() {
+
+	// On board LED (useful for debugging)
+	pinMode(PICO_LED_PIN, OUTPUT);
+
+	// Turn the on board LED on
+	digitalWrite(PICO_LED_PIN, 1);
+
+	bg.enableColorCorrection(true);
+	matrix.addLayer(&bg);
+	matrix.setBrightness(255);
+
+	// Init the library and the matrix
+	matrix.begin();
+
+	// -------------------------------------------------------------------------
+
+	// Draw a red pixel at 0, 0 (top left)
+	bg.drawPixel(0, 0, {255, 0, 0});
+
+	// Draw a green pixel at 10, 0
+	bg.drawPixel(10, 0, {0, 255, 0});
+
+	// Draw other things...
+	bg.fillRectangle(12, 15, 30, 20, {200, 0, 180});
+	bg.drawLine(10, 10, 30, 30, {255, 127, 0});
+
+	// Display the draw buffer
+	bg.swapBuffers(false);
+
+}
+
+void loop() {
+
+}
